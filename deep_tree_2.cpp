468b4cf0e80dba8013dd0673bee9b1b98f2ef1aa
Soyuz-Apollo <53566941+Soyuz-Apollo@users.noreply.github.com>
2019-11-08 17:36:14 +0300

    Update deep_tree_2.cpp

--- deep_tree_2.cpp
+++ deep_tree_2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <queue>
+
 
 using namespace std;
 
@@ -38,12 +40,56 @@ int main(){
 		flag = true;
 	}
 
-	for(int i = 0; i < tree.size(); i++){
+	/*for(int i = 0; i < tree.size(); i++){
 		cout <<" root "<< tree[i].front()<<endl;
 		cout <<" childrens ";
-		for(list<int>::iterator it = tree[i].begin()+1; it != tree[i].end(); it++){
+		for(list<int>::iterator it = tree[i].begin(); it != tree[i].end(); it++){
 			cout << *it <<" ";
 		}
 		cout << endl;
 	}
+	 */
+
+	int deep = 1;
+	int numV1 = 0;
+	int numV2 = 0;
+	queue<int> verx;
+
+	for(vector<list<int>> :: iterator flash = tree.begin(); flash < tree.end(); flash++){
+		if((*flash).front() == root){
+			(*flash).pop_front();
+			while(!(*flash).empty()){
+				verx.push((*flash).front());
+				(*flash).pop_front();
+				numV1++;
+			}
+			tree.erase(flash);
+		}
+	}
+
+	while(!verx.empty()){
+		while(numV1 > 0){
+			root = verx.front();
+			for (vector<list<int>> :: iterator flash = tree.begin(); flash < tree.end(); flash ++){
+				if((*flash).front() == root){
+					(*flash).pop_front();
+					while(!(*flash).empty()){
+						verx.push((*flash).front());
+						(*flash).pop_front();
+						numV2++;
+					}
+					tree.erase(flash);
+				}
+			}
+			numV1--;
+			verx.pop();
+		}
+		numV1 = numV2;
+		numV2 = 0;
+		deep++;
+	}
+
+	cout << deep << endl;
+
 }
+
